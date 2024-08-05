50a0ef0a71812df62ebcb74d01b5f9db297743b1
vinayak  gohel <104017167+Virstriker@users.noreply.github.com>
2024-08-05 22:32:48 +0530

    Creating new project

---
+++ src/main/java/com/microservices/dynmic_data_hub/DynmicDataHubApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.dynmic_data_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynmicDataHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynmicDataHubApplication.class, args);
+	}
+
+}
